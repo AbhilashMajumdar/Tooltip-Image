cb8ec6d88f8d792acf8b224c737d96d413616bc5
Abhilash Majumdar <87374472+AbhilashMajumdar@users.noreply.github.com>
2021-09-05 13:14:32 +0530

    Add files via upload

---
+++ TooltipScript.js
@@ -0,0 +1,17 @@
+let tip=document.getElementById("tip");
+
+function laptop(){
+    tip.innerHTML = "This is a Laptop";
+}
+
+function mobile(){
+    tip.innerHTML = "This is a Mobile";
+}
+
+function coffieMug(){
+    tip.innerHTML = "This is a CoffieMug";
+}
+
+function diary(){
+    tip.innerHTML = "This is a Diary";
+}
